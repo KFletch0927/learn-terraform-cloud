2d74649f7d23d69bfe3cc2d17a34f62687ce2dcc
Kiahl Fletcher <kiahl.fletcher@effectual.com>
2022-10-21 13:41:32 -0400

    Add auto.tfvars file

--- terraform.tf
+++ terraform.tf
@@ -1,12 +1,12 @@
 terraform {
 
-#  cloud {
+/*  cloud {
     organization = "hashicorp-kf-effectual"
 
     workspaces {
       name = "learn-terraform-cloud"
     }
-#  }
+/*  }
 
   required_providers {
     aws = {
