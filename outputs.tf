2fac3fc9e48eb8e84c11b4f40116a1132c0646d4
KFletch0927 <58040034+KFletch0927@users.noreply.github.com>
2023-03-15 07:52:51 -0400

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,8 +1,14 @@
-output "instance_ami" {
-  value = aws_instance.ubuntu.ami
+# Define Output Values
+
+# Attribute Reference
+output "ec2_instance_publicip" {
+  description = "EC2 Instance Public IP"
+  value = aws_instance.my-ec2-vm.*.public_ip
 }
 
-output "instance_arn" {
-  value = aws_instance.ubuntu.arn
-}
 
+# Attribute Reference - Create Public DNS URL
+output "ec2_publicdns" {
+  description = "Public DNS URL of an EC2 Instance"
+  value = aws_instance.my-ec2-vm.*.public_dns
+}
